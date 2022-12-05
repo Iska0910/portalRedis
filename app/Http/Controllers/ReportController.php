80126c469650c8bd584dcb223605a9fbdaaca20e
Ysmayyl <Ysmayylhan0910@gmail.com>
2022-12-05 18:09:30 +0500

    Fiex promble with date dilter

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Blog;
 use App\Models\Worker;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class ReportController extends Controller
 {
@@ -24,35 +25,31 @@ class ReportController extends Controller
 
     public function workerDetail(Request $request, $id)
     {
-
         $worker = Worker::query()
             ->find((int)$id);
 
-
+        $begin = Carbon::parse($request->start)->startOfDay();
+        $end = Carbon::parse($request->end)->endOfDay();
 
         $blogs = Blog::query()
-            ->when($request->start, function ($q, $v){
-                $q->where('date_added', '>=', $v);
+            ->when($request->start, function ($q) use ($begin){
+                $q->whereDate('date_added', '>=', $begin);
             })
-            ->when($request->end, function ($q, $v){
-                $q->where('date_added', '<=', $v);
+            ->when($request->end, function ($q) use ($end){
+                $q->whereDate('date_added', '<=', $end);
             })
-            ->where('worker_ru',  $worker->id)
-            ->orWhere('worker_tm', '=', $worker->id)
-            ->orWhere('worker_en', '=', $worker->id)
+            ->where(function ($q) use ($worker) {
+                $q->where('worker_ru',  $worker->id)
+                    ->orwhere('worker_tm', '=', $worker->id)
+                    ->orWhere('worker_en', '=', $worker->id);
+            })
             ->select('id', 'title_ru','title_tm', 'visited_count as view_count', 'status', 'date_added as created_at')
             ->with('viewsDetail')
             ->orderBy('date_added', 'desc')
             ->paginate(15);
 
-        $count = Blog::query()
-            ->where('worker_ru',  $worker->id)
-            ->orWhere('worker_tm', '=', $worker->id)
-            ->orWhere('worker_en', '=', $worker->id)
-            ->count();
-
         session()->flashInput($request->input());
 
-        return view('report.worker-detail', compact('blogs', 'count', 'worker'));
+        return view('report.worker-detail', compact('blogs', 'worker'));
     }
 }
