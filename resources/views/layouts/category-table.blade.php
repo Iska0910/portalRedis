7341b7889bdbf94a890d7225043823740aca0c6c
Ysmayyl <Ysmayylhan0910@gmail.com>
2022-12-10 19:57:31 +0500

    some changes

--- resources/views/layouts/category-table.blade.php
+++ resources/views/layouts/category-table.blade.php
@@ -1,40 +1,40 @@
-<div class="row d-flex align-items-center" style="font-weight: bold; margin-top: 20px; border-bottom: 2px solid #dee2e6; border-top: 2px solid #dee2e6; padding: 10px 0;">
+<div class="row d-flex align-items-center" style="font-weight: bold; margin-top: 20px; border-bottom: 3px solid #dee2e6; border-top: 3px solid #dee2e6; padding: 10px 0;">
     <div class="col-1">№</div>
     <div class="col-2 d-flex justify-content-center">Title</div>
     <div class="col-1 d-flex justify-content-center">Status</div>
     <div class="col-1 d-flex justify-content-center">Views</div>
-    <div class="col-2" style="border-left: 2px solid rgb(222, 226, 230); border-right: 2px solid rgb(222, 226, 230);">
+    <div class="col-2" style="border-left: 3px solid rgb(222, 226, 230); border-right: 3px solid rgb(222, 226, 230);">
         <div class="d-flex justify-content-center mb-1">
             <img src="{{asset('storage/ru.png')}}">
         </div>
-        <div class="row">
-            <div style="text-align: center;" class="col">Worker</div>
-            <div style="text-align: center;" class="col">View</div>
+        <div class="row mt-3">
+            <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8"><i class="fa fa-user"></i></div>
+            <div style="text-align: center;" class="col-4"><i style="color: #528dd7" class="fa fa-eye"></i></div>
         </div>
     </div>
     <div class="col-2">
         <div class="d-flex justify-content-center mb-1">
             <img src="{{asset('storage/tm.png')}}">
         </div>
-        <div class="row">
-            <div style="text-align: center;" class="col">Worker</div>
-            <div style="text-align: center;" class="col">View</div>
+        <div class="row mt-3">
+            <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8"><i class="fa fa-user"></i></div>
+            <div style="text-align: center;" class="col-4"><i style="color: #528dd7" class="fa fa-eye"></i></div>
         </div>
     </div>
-    <div class="col-2" style="border-left: 2px solid rgb(222, 226, 230); border-right: 2px solid rgb(222, 226, 230);">
+    <div class="col-2" style="border-left: 3px solid rgb(222, 226, 230); border-right: 3px solid rgb(222, 226, 230);">
         <div class="d-flex justify-content-center mb-1">
             <img src="{{asset('storage/en.png')}}">
         </div>
-        <div class="row">
-            <div style="text-align: center;" class="col">Worker</div>
-            <div style="text-align: center;" class="col">View</div>
+        <div class="row mt-3">
+            <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8"><i class="fa fa-user"></i></div>
+            <div style="text-align: center;" class="col-4"><i style="color: #528dd7" class="fa fa-eye"></i></div>
         </div>
     </div>
     <div class="col-1 d-flex justify-content-center"><i style="color: #49e309" class="fa fa-link"></i></div>
 </div>
 
 @foreach($datas as $data)
-    <div class="row d-flex align-items-center" style="border-bottom: 1px solid #dee2e6; padding: 10px 0;">
+    <div class="row d-flex align-items-center" style="border-bottom: 0.5px solid #dee2e6; padding: 10px 0;">
         <div style="font-weight: bold;" class="col-1">{{$loop->iteration}}</div>
         <div class="col-2 d-flex justify-content-center" style="text-align: center">
             @if($data->title_ru){{$data->title_ru}}@else{{$data->title_tm}}@endif
@@ -47,40 +47,40 @@
             @endif
         </div>
         <div class="col-1 d-flex justify-content-center">{{$data->views}}</div>
-        <div class="col-2" style="border-left: 2px solid rgb(222, 226, 230); border-right: 2px solid rgb(222, 226, 230);">
-            <div class="row">
-                <div style="text-align: center; border-right: 1px solid rgb(222, 226, 230);" class="col-8">
+        <div class="col-2" style="border-left: 3px solid rgb(222, 226, 230); border-right: 3px solid rgb(222, 226, 230);">
+            <div class="row h-100">
+                <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8 h-100">
                     @if(isset($worker[$data->worker_ru]))
                     {{$worker[$data->worker_ru]}}
                     @else
                         Didn't select
                     @endif
                 </div>
-                <div style="text-align: center;" class="col-4">@if(isset($data->viewsDetail)) {{$data->viewsDetail->ru}} @endif</div>
+                <div style="text-align: center;" class="col-4 h-100">@if(isset($data->viewsDetail)) {{$data->viewsDetail->ru}} @endif</div>
             </div>
         </div>
         <div class="col-2">
-            <div class="row">
-                <div style="text-align: center; border-right: 1px solid rgb(222, 226, 230);" class="col-8">
+            <div class="row h-100">
+                <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8 h-100">
                     @if(isset($worker[$data->worker_tm]))
                     {{$worker[$data->worker_tm]}}
                     @else
                         Didn't select
                     @endif
                 </div>
-                <div style="text-align: center;" class="col-4">@if(isset($data->viewsDetail)) {{$data->viewsDetail->tm}} @endif</div>
+                <div style="text-align: center;" class="col-4 h-100">@if(isset($data->viewsDetail)) {{$data->viewsDetail->tm}} @endif</div>
             </div>
         </div>
-        <div class="col-2" style="border-left: 2px solid rgb(222, 226, 230); border-right: 2px solid rgb(222, 226, 230);">
-            <div class="row">
-                <div style="text-align: center; border-right: 1px solid rgb(222, 226, 230);" class="col-8">
+        <div class="col-2" style="border-left: 3px solid rgb(222, 226, 230); border-right: 3px solid rgb(222, 226, 230);">
+            <div class="row h-100">
+                <div style="text-align: center; border-right: 0.5px solid rgb(222, 226, 230);" class="col-8 h-100">
                     @if(isset($worker[$data->worker_en]))
                     {{$worker[$data->worker_en]}}
                     @else
                         Didn't select
                     @endif
                 </div>
-                <div style="text-align: center;" class="col-4">@if(isset($data->viewsDetail)) {{$data->viewsDetail->en}} @endif</div>
+                <div style="text-align: center;" class="col-4 h-100">@if(isset($data->viewsDetail)) {{$data->viewsDetail->en}} @endif</div>
             </div>
         </div>
         <div class="col-1 d-flex justify-content-center">
